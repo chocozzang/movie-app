8ab962d089209616ab929a118c40d4252985bd82
Jinho <jangjh_05@naver.com>
2023-07-10 10:42:00 +0900

    api로 데이터를 받음

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieList from "./components/MovieList";
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      Title: "The Amazing Spider-Man",
-      Year: "2012",
-      imdbID: "tt0948470",
-      Type: "movie",
-      Poster: "https://m.media-amazon.com/images/M/MV5BMjMyOTM4MDMxNV5BMl5BanBnXkFtZTcwNjIyNzExOA@@._V1_SX300.jpg"
-    },
-    {
-      Title: "The Amazing Spider-Man 2",
-      Year: "2014",
-      imdbID: "tt1872181",
-      Type: "movie",
-      Poster: "https://m.media-amazon.com/images/M/MV5BOTA5NDYxNTg0OV5BMl5BanBnXkFtZTgwODE5NzU1MTE@._V1_SX300.jpg"
-    },
-    {
-      Title: "The Amazing World of Gumball",
-      Year: "2011–2019",
-      imdbID: "tt1942683",
-      Type: "series",
-      Poster: "https://m.media-amazon.com/images/M/MV5BYWU1YTA4OGUtNjcxMC00ZTllLTgxYWUtY2U5NmViZTU0MmNjXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_SX300.jpg"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
+
+
+  // 검색어로 영화데이터 요청
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=8ccebf15`;
+    // 자바스크립트는 비동기 구동임
+    // 즉, url로 api를 요청해서 다 받기 전에 실행함
+    // 따라서, await를 이용해서 요청이 끝날 때 까지 기다리도록 함
+    // await 사용시 async를 붙여야함..
+    const response = await fetch(url);
+
+    const responseJson = await response.json();
+    setMovies(responseJson.Search);
+    console.log(responseJson);
+  };
+
+  useEffect(() => {
+    getMovieRequest('star wars');
+  }, []);
+
   return (
-    <div>
-      헬로우
+    <div className='container-fluid movie-app'>
+      <div className='row'>
+        <MovieList movies = {movies}/>
+      </div>
     </div>
   );
 }
